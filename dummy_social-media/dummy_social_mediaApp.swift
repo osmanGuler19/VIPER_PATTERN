a8e96debe79ad1b15eca595372602944a8a51096
osmanGuler19 <osman-guler@outlook.com>
2023-04-30 15:18:10 +0300

    Initial Commit

---
+++ dummy_social-media/dummy_social_mediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummy_social_mediaApp.swift
+//  dummy_social-media
+//
+//  Created by Osman Güler on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct dummy_social_mediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
